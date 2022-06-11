0c0189b21e4bc22e7db6e62cded167332d4c1d76
MirandaRG45 <97415240+MirandaRG45@users.noreply.github.com>
2022-06-11 15:18:17 -0500

    Update behaviour.js

--- behaviour.js
+++ behaviour.js
@@ -1,77 +1,116 @@
 const formulario = $("#formulario"); //guarda TODO el FORMULARIO en una constante
 const inputs = $('#formulario input'); //guarda TODAS las INPUTS dentro del formulario
 const expresiones = {
+  //Datos del niño
   boleta: /^(\d{10}|(PE|PP)\d{8})$/,
   nombre: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
   apePaterno: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
   apeMaterno: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
   curp: /^([A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2})$/,
-  correo: /^([a-záéíóúñüA-ZÁÉÍÓÚÑÜ0-9.\-_/\\]+@\w+(\.\w+)+)$/,
-  nombreDH: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
+  //Datos del derecho habiente
   apePaternoDH: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
   apeMaternoDH: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
+  nombreDH: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
   curpDH: /^([A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2})$/,
+  calle: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9]+\s?){1,4})$/,
+  noInt: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9]+\s?){1,2})$/,
+  noExt: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9]+\s?){1,2})$/,
+  colonia: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9-_.]+\s?){1,4})$/,
+  municipio: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ]+\s?){1,5})$/,
+  cp: /^\d{5}$/,
   telFijo: /^[0-9]{10}$/,
   telCel: /^[0-9]{10}$/,
-  nombreCon: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
+  correo: /^([a-záéíóúñüA-ZÁÉÍÓÚÑÜ0-9.\-_/\\]+@\w+(\.\w+)+)$/,
+  puesto: /^([A-ZÁÉÍÓÑÚa-záéíóúñ]+\s?)+$/,
+  saldo: /^\d{4,5}$/,
+  noEmp: /^\d{5,7}$/,
+  ext: /^\d{5}$/,
+  //Datos del conyuge
   apePaternoCon: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
   apeMaternoCon: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
-  curpCon: /^([A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2})$/,
-  telTrabajoCon: /^[0-9]{10}$/
-
+  nombreCon: /^((([A-ZÁÉÍÓÚÑ]{1})[a-záéíóúñ]+\s?){1,3})$/,
+  trabajoCon: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9-_]+\s?){1,5})$/,
+  telTrabajoCon: /^[0-9]{10}$/,
+  calleTraCon: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9]+\s?){1,4})$/,
+  noExtCon: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9]+\s?){1,2})$/,
+  noIntCon: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9]+\s?){1,2})$/,
+  coloniaCon: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ0-9-_.]+\s?){1,4})$/,
+  municipioCon: /^(([A-ZÁÉÍÓÑÚa-záéíóúñ]+\s?){1,5})$/,
+  extensionCon: /^\d{4,5}$/,
+  curpCon: /^([A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2})$/
 }
 
 const campos = {
+  //Datos del niño
   boleta: false,
   nombre: false,
   apePaterno: false,
   apeMaterno: false,
   curp: false,
-  correo: false,
-  nombreDH: false,
+  //Datos del derecho habiente
   apePaternoDH: false,
   apeMaternoDH: false,
+  nombreDH: false,
   curpDH: false,
+  calle: false,
+  noInt: false,
+  noExt: false,
+  colonia: false,
+  municipio: false,
+  cp: false,
   telFijo: false,
   telCel: false,
-  nombreCon: false,
+  correo: false,
+  puesto: false,
+  saldo: false,
+  noEmp: false,
+  ext: false,
+  //datos del conyuge
   apePaternoCon: false,
   apeMaternoCon: false,
-  curpCon: false,
-  telTrabajoCon: false
+  nombreCon: false,
+  trabajoCon: false,
+  telTrabajoCon: false,
+  calleTraCon: false,
+  noExtCon: false,
+  noIntCon: false,
+  coloniaCon: false,
+  municipioCon: false,
+  extensionCon: false,
+  curpCon: false
 }
 
 $(function() {
   formulario.submit(function(event) {
     event.preventDefault();
 
-      if ($('.no').prop('checked')) {
-        if (campos.boleta && campos.apePaterno && campos.apeMaterno && campos.nombre && campos.curp && campos.correo &&
-          campos.nombreDH && campos.apePaternoDH && campos.apeMaternoDH && campos.curpDH &&
-          campos.telFijo && campos.telCel) {
-            verificaConyuge();
-          } else {
-            $(".formularioMensajeError").addClass("formularioMensajeError-Activo")
-          }
+    if ($('.no').prop('checked')) {
+      if (campos.boleta && campos.nombre && campos.apePaterno && campos.apeMaterno && campos.curp &&
+        campos.apePaternoDH && campos.apeMaternoDH && campos.nombreDH && campos.curpDH && campos.calle && campos.noInt &&
+        campos.noExt && campos.colonia && campos.municipio && campos.cp && campos.telFijo && campos.telCel &&
+        campos.correo && campos.puesto && campos.saldo && campos.noEmp && campos.ext) {
+        verificaConyuge();
       } else {
-        if (campos.boleta && campos.apePaterno && campos.apeMaterno && campos.nombre && campos.curp && campos.correo &&
-          campos.nombreDH && campos.apePaternoDH && campos.apeMaternoDH && campos.curpDH &&
-          campos.telFijo && campos.telCel && campos.apeMaternoCon && campos.apePaternoCon &&
-        campos.curpCon && campos.telTrabajoCon) {
-          verificaConyuge();
-        } else {
-          $(".formularioMensajeError").addClass("formularioMensajeError-Activo")
-        }
+        $(".formularioMensajeError").addClass("formularioMensajeError-Activo")
       }
-
-
-
-
+    } else {
+      if (campos.boleta && campos.nombre && campos.apePaterno && campos.apeMaterno && campos.curp &&
+        campos.apePaternoDH && campos.apeMaternoDH && campos.nombreDH && campos.curpDH && campos.calle && campos.noInt &&
+        campos.noExt && campos.colonia && campos.municipio && campos.cp && campos.telFijo && campos.telCel &&
+        campos.correo && campos.puesto && campos.saldo && campos.noEmp && campos.ext && campos.apePaternoCon &&
+        campos.apeMaternoCon && campos.nombreCon && campos.trabajoCon && campos.telTrabajoCon && campos.calleTraCon &&
+        campos.noExtCon && campos.noIntCon && campos.coloniaCon && campos.municipioCon && campos.extensionCon &&
+        campos.curpCon) {
+        verificaConyuge();
+      } else {
+        $(".formularioMensajeError").addClass("formularioMensajeError-Activo")
+      }
+    }
   });
 
   $('.datepicker').datepicker({ //materialize CALENDARIO
     minDate: new Date(2015, 5, 14),
-    maxDate: new Date(2022,5,10),
+    maxDate: new Date(2022, 5, 10),
     autoClose: true
   });
   $('select').formSelect(); //materialize SELECT
@@ -103,14 +142,14 @@ function validarFormulario(e) {
     case "boleta":
       validarCampo(expresiones.boleta, e.target, e.target.name);
       break;
+    case "nombre":
+      validarCampo(expresiones.nombre, e.target, e.target.name);
+      break;
     case "apePaterno":
       validarCampo(expresiones.apePaterno, e.target, e.target.name);
       break;
     case "apeMaterno":
       validarCampo(expresiones.apeMaterno, e.target, e.target.name);
-      break;
-    case "nombre":
-      validarCampo(expresiones.nombre, e.target, e.target.name);
       break;
     case "curp":
       validarCampo(expresiones.curp, e.target, e.target.name);
@@ -127,6 +166,24 @@ function validarFormulario(e) {
     case "curpDH":
       validarCampo(expresiones.curpDH, e.target, e.target.name);
       break;
+    case "calle":
+      validarCampo(expresiones.calle, e.target, e.target.name);
+      break;
+    case "noInt":
+      validarCampo(expresiones.noInt, e.target, e.target.name);
+      break;
+    case "noExt":
+      validarCampo(expresiones.noExt, e.target, e.target.name);
+      break;
+    case "colonia":
+      validarCampo(expresiones.colonia, e.target, e.target.name);
+      break;
+    case "municipio":
+      validarCampo(expresiones.municipio, e.target, e.target.name);
+      break;
+    case "cp":
+      validarCampo(expresiones.cp, e.target, e.target.name);
+      break;
     case "telFijo":
       validarCampo(expresiones.telFijo, e.target, e.target.name);
       break;
@@ -135,6 +192,18 @@ function validarFormulario(e) {
       break;
     case "correo":
       validarCampo(expresiones.correo, e.target, e.target.name);
+      break;
+    case "puesto":
+      validarCampo(expresiones.puesto, e.target, e.target.name);
+      break;
+    case "saldo":
+      validarCampo(expresiones.saldo, e.target, e.target.name);
+      break;
+    case "noEmp":
+      validarCampo(expresiones.noEmp, e.target, e.target.name);
+      break;
+    case "ext":
+      validarCampo(expresiones.ext, e.target, e.target.name);
       break;
     case "apePaternoCon":
       validarCampo(expresiones.apePaternoCon, e.target, e.target.name);
@@ -145,8 +214,29 @@ function validarFormulario(e) {
     case "nombreCon":
       validarCampo(expresiones.nombreCon, e.target, e.target.name);
       break;
+    case "trabajoCon":
+      validarCampo(expresiones.trabajoCon, e.target, e.target.name);
+      break;
+    case "calleTraCon":
+      validarCampo(expresiones.calleTraCon, e.target, e.target.name);
+      break;
     case "telTrabajoCon":
       validarCampo(expresiones.telTrabajoCon, e.target, e.target.name);
+      break;
+    case "noExtCon":
+      validarCampo(expresiones.noExtCon, e.target, e.target.name);
+      break;
+    case "noIntCon":
+      validarCampo(expresiones.noIntCon, e.target, e.target.name);
+      break;
+    case "coloniaCon":
+      validarCampo(expresiones.coloniaCon, e.target, e.target.name);
+      break;
+    case "municipioCon":
+      validarCampo(expresiones.municipioCon, e.target, e.target.name);
+      break;
+    case "extensionCon":
+      validarCampo(expresiones.extensionCon, e.target, e.target.name);
       break;
     case "curpCon":
       validarCampo(expresiones.curpCon, e.target, e.target.name);
@@ -191,19 +281,19 @@ function calcularEdad() {
   $('#meses').val(m);
 }
 
-function verificaConyuge(){
-    formulario[0].reset();
+function verificaConyuge() {
+  formulario[0].reset();
 
-    $(".mensajeExito").addClass("mensajeExito-Activo");
-    setTimeout(function(){
-      $(".mensajeExito").hide();
-      $("i").each(function(icono){
-        icono.remove();
-      });
-    },3000);
-
-    $("div i").each(function(icono) {
-      icono.removeClass("fa-check-circle");
-      icono.addClass("formularioValidacion");
+  $(".mensajeExito").addClass("mensajeExito-Activo");
+  setTimeout(function() {
+    $(".mensajeExito").hide();
+    $("i").each(function(icono) {
+      icono.remove();
     });
+  }, 3000);
+
+  $("div i").each(function(icono) {
+    icono.removeClass("fa-check-circle");
+    icono.addClass("formularioValidacion");
+  });
 }
