ddea5a1ba0151612ddd2b26089ba82b1557ec556
MirandaRG45 <97415240+MirandaRG45@users.noreply.github.com>
2022-06-12 06:43:34 -0500

    Add files via upload

---
+++ getForm.php
@@ -0,0 +1,11 @@
+<?php
+
+ $nombreDH = $_POST["nombreDH"];
+ $apePaternoDH = $_POST["apePaternoDH"];
+ $apeMaternoDH = $_POST["apeMaternoDH"];
+
+ echo "<body style='background-color:#DAEAF1;'><h1 style='margin-top:300px;'><center>
+ <span style='color:blue;'> $nombreDH $apePaternoDH $apeMaternoDH </span>, tus datos fueron guardados.
+ </center></h1></body>";
+
+ ?>
